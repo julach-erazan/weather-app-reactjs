2774fd87ea7888232a9fcbfa74a9e0b53b373b28
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-06 15:42:25 +0530

    Update current_weatherApi.js

--- src/api/current_weatherApi.js
+++ src/api/current_weatherApi.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-const BASE_URL = "http://api.weatherapi.com/v1";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const BASE_URL = process.env.BASE_URL;
+
 export const getWeatherByLocation = async (location) => {
   try {
     const response = await axios.get(`${BASE_URL}/forecast.json`, {
       params: {
         q: location,
-        key: "70f174cd7b3e4cfcb6f180917252701",
+        key: API_KEY,
         days: "8",
       },
     });
