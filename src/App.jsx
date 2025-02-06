89980a3883498f82f4ae2ef4cfcd898a428c818d
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-06 22:59:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Home from "./pages/Home";
 import GetStart from "./pages/GetStart";
 import EnableLocation from "./pages/EnableLocation";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/get-start" element={<GetStart />} />
         <Route path="/enable-location" element={<EnableLocation />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
