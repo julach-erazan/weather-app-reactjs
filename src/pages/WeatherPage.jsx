c47ca3ea495fa2a609e7009a5fabed8701a5dcbe
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-06 13:04:43 +0530

    Create WeatherPage.jsx

---
+++ src/pages/WeatherPage.jsx
@@ -0,0 +1,39 @@
+import {React, useState, useEffect} from "react";
+import WeatherCard from "../components/WeatherCard";
+import SearchButton from "../components/SearchButton"
+import { connect } from 'react-redux';
+import ForecastCard from "../components/ForecastCard"
+
+const WeatherPage = (props) => {
+
+  const [viewForecast, setViewForecast] = useState(false);
+
+  useEffect(() => {
+    if(props.AllForecastButton != null){
+      setViewForecast(props.AllForecastButton)
+    }
+  })
+
+  return (
+    <div className="p-4 flex flex-col items-center">
+      {viewForecast ? (
+        <>
+          <ForecastCard/>
+        </>
+      ): (
+        <>
+          <SearchButton/>
+          <WeatherCard />
+        </>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    AllForecastButton: state.allForecastButton,
+  };
+};
+
+export default connect(mapStateToProps)(WeatherPage);
