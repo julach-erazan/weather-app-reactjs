cd80b78b8e2f0ff5f5b6faa9630987d1b796772f
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-06 13:04:40 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherPage from './WeatherPage';
+
+
+const Home = () => {
+
+  return (
+    <div className="bg-gradient-to-br from-white to-[#e9effd] min-h-screen">
+        <WeatherPage/>
+    </div>
+  );
+}
+
+export default Home;
